c3354e98d3c8d5359d05ea74ab1bfd4961da131a
Mark deVilliers <markdevilliers@gmail.com>
2013-11-13 10:54:47 +0000

    Updating vagrant file to Vagrant 2, reconfigured esd

--- Vagrantfile
+++ Vagrantfile
@@ -1,9 +1,12 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-Vagrant::Config.run do |config|
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
   config.vm.box = "precise64"
-  config.vm.provision :shell, :path => "provision.sh"
+  config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+  config.vm.provision 'shell', path: 'provision.sh'
   
 end
--- provision.sh
+++ provision.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive 
 
-cat >> /etc/apt/sources.list <<EOL
-deb http://binaries.erlang-solutions.com/debian quantal contrib
-deb http://binaries.erlang-solutions.com/debian precise contrib
-deb http://binaries.erlang-solutions.com/debian oneiric contrib
-deb http://binaries.erlang-solutions.com/debian natty contrib
-deb http://binaries.erlang-solutions.com/debian maverick contrib
-deb http://binaries.erlang-solutions.com/debian lucid contrib
-deb http://binaries.erlang-solutions.com/debian wheezy contrib
-deb http://binaries.erlang-solutions.com/debian squeeze contrib
-EOL
-
-wget -O - http://binaries.erlang-solutions.com/debian/erlang_solutions.asc | apt-key add -
+wget http://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+sudo dpkg -i erlang-solutions_1.0_all.deb
 
 apt-get update > /dev/null
 
 apt-get -y install make curl g++ mercurial git zip xsltproc esl-erlang
+
